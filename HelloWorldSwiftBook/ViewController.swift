407c4fb0e147fc039e944cfdb6b2bf7bb220d843
Alexander6Shevtsov <axrsvr@gmail.com>
2024-03-13 12:11:52 +0400

    Setup UI

--- HelloWorldSwiftBook/ViewController.swift
+++ HelloWorldSwiftBook/ViewController.swift
@@ -6,11 +6,34 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+// финалим класс т.к он последний, название нужно сменить
+final class ViewController: UIViewController {
+    // ! в конце, эту var нужно иниц до обращения
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var showGreetingButton: UIButton!
+    
+    
+    // метод унаследован от супер класса UIViewController
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad() // обращаемся к род классу и вызываем у него метод
+        greetingLabel.isHidden.toggle() // скрываем приветствие
+        showGreetingButton.layer.cornerRadius = 10 // скругление кнопки
     }
+    // (_ sender: UIButton) убрали параметрs метода т.к есть свойство (13)
+    @IBAction func showGreetingButtonDidTapped() {
+        greetingLabel.isHidden.toggle()
+        showGreetingButton.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal
+        )
+
+ /* юзаем тернарный вместо if else
+        if greetingLabel.isHidden {
+            sender.setTitle("Show Greeting", for: .normal)
+        } else {
+            sender.setTitle("Hide Greeting", for: .normal)
+       }
+ */
+    }
+    
 }
-// Vasia Rogov
